9aea5e877cd62664f1b1847f2c743814fba1d9cc
Karol Śmigielski <karol.smigielski@teb.edu.pl>
2024-02-12 09:31:19 +0100

    lata do przodu

--- Assets/scripts/PlayerControler.cs
+++ Assets/scripts/PlayerControler.cs
@@ -13,6 +13,20 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //dodaj do wspó³rzêdnych wartoœæ x=1, y=0, z =0 * czas w mierzony w sekundy ostatniej klatki
+        //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+
+        //prezentacja dzia³ania wyg³adzonego sterowania(emualcja joysticka)
+        //Debug.Log(Input.GetAxis("Vertical"));
+
+        //sterowanie prêdkoœi¹
+        //stwórz nowy vector przesuniêcia o wartoœæ 1 do przodu
+        Vector3 movment = Vector3.forward;
+        //pomnó¿ przez czas 
+        movment*= Time.deltaTime;
+        //pomnó¿ go przez "wychylenie joysticka"
+        movment *= Input.GetAxis("Vertical");
+        //dodaj ruch obiektu
+        transform.position += movment;
     }
 }
