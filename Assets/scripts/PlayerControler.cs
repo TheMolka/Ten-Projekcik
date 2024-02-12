c480299c5f7106ae436f0c40c35aad7324eb35e0
Karol Śmigielski <karol.smigielski@teb.edu.pl>
2024-02-12 10:29:59 +0100

    mozna sterowac

--- Assets/scripts/PlayerControler.cs
+++ Assets/scripts/PlayerControler.cs
@@ -5,43 +5,49 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     public float rotationSpeed = 100f;
+    public float flySpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //dodaj do współrzędnych wartość x=1, y=0, z =0 * czas w mierzony w sekundy ostatniej klatki
+        //dodaj do wspó³rzêdnych wartoœæ x=1, y=0, z=0 pomno¿one przez czas
+        //mierzony w sekundach od ostatniej klatki
         //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
 
-        //prezentacja działania wygładzonego sterowania(emualcja joysticka)
+        //prezentacja dzia³ania wyg³adzonego sterowania (emualcja joystika)
         //Debug.Log(Input.GetAxis("Vertical"));
 
-        //sterowanie prędkośią
-        //stwórz nowy vector przesunięcia o wartość 1 do przodu
-        Vector3 movment = Vector3.forward;
-        //pomnóż przez czas 
-        movment*= Time.deltaTime;
-        //pomnóż go przez "wychylenie joysticka"
-        movment *= Input.GetAxis("Vertical");
-        //dodaj ruch obiektu
-        transform.position += movment;
+        //sterowanie prêdkoœci¹
+        //stworz nowy wektor przesuniêcia o wartoœci 1 do przodu
+        Vector3 movement = transform.forward;
+        //pomnó¿ go przez czas od ostatniej klatki
+        movement *= Time.deltaTime;
+        //pomnó¿ go przez "wychylenie joystika"
+        movement *= Input.GetAxis("Vertical");
+        //pomnó¿ przez prêdkoœæ lotu
+        movement *= flySpeed;
+        //dodaj ruch do obiektu
+        transform.position += movement;
+
 
         //obrót
-        //modyfikuj  "Y" obiektu player
+        //modyfikuj oœ "Y" obiektu player
         Vector3 rotation = Vector3.up;
         //przemnó¿ przez czas
         rotation *= Time.deltaTime;
         //przemnó¿ przez klawiaturê
         rotation *= Input.GetAxis("Horizontal");
-        //pomnóż prędkość obrotu
+        //pomnó¿ przez prêdkoœæ obrotu
         rotation *= rotationSpeed;
         //dodaj obrót do obiektu
-        //nie mozemy uzywac += poniewa¿ unity u¿ywa Quaternionów do zapisu rotacji
+        //nie mo¿emy u¿yæ += poniewa¿ unity u¿ywa Quaternionów do zapisu rotacji
         transform.Rotate(rotation);
+
 
     }
 }
